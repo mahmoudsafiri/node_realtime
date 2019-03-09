93fc4a4d13424dd705078bcbd8cd07b2fa08d850
mahmoud safiri <alirezasafiri@outlook.com>
2019-03-09 16:02:16 -0500

    cloud.mongo

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ var Message = mongoose.model('Message',{
   name : String,
   message : String
 })
+
+const MongoClient = require(‘mongodb’).MongoClient;
+const uri = "mongodb+srv://admin:1234@cluster0-tjov6.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("real_time").collection("messages");
+  // perform actions on the collection object
+  client.close();
+});
+
 // var dbUrl = 'mongodb://username:password@ds257981.mlab.com:57981/simple-chat'
 app.get('/messages', (req, res) => {
   Message.find({},(err, messages)=> {
@@ -33,7 +43,7 @@ io.on('connection', () =>{
 // mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
 //   console.log('mongodb connected',err);
 // })
-mongoose.connect("mongodb://localhost:27017/real_time");
+// mongoose.connect("mongodb://localhost:27017/real_time");
 const port = process.env.PORT || 3000;
 var server = http.listen(port, () => {
   console.log('server is running on port', server.address().port);
