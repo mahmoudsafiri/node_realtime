65155209e36ec5fb81fd87b0ad8ab8afe035a967
mahmoud safiri <alirezasafiri@outlook.com>
2020-01-19 08:32:22 -0500

    y

--- server.js
+++ server.js
@@ -24,20 +24,20 @@ var User = mongoose.model('User',{
 ///////////////////Auth
 var passport = require('passport'),LocalStrategy = require('passport-local').Strategy;
 //
-// passport.use(new LocalStrategy(
-//   function(username, password, done) {
-//     User.findOne({ username: username }, function(err, user) {
-//       if (err) { return done(err); }
-//       if (!user) {
-//         return done(null, false, { message: 'Incorrect username.' });
-//       }
-//       if (!user.validPassword(password)) {
-//         return done(null, false, { message: 'Incorrect password.' });
-//       }
-//       return done(null, user);
-//     });
-//   }
-// ));
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 // app.get('/login', function (req, res)
 // {
 //     res.render('login.html');
