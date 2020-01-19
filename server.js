866426f637eb642b70d0597986ce78c097faaf74
mahmoud safiri <alirezasafiri@outlook.com>
2020-01-19 08:16:05 -0500

    y

--- server.js
+++ server.js
@@ -13,42 +13,41 @@ var Message = mongoose.model('Message',{
   name : String,
   message : String,
   updated_at: { type: Date, default: Date.now },
-})
+});
 var User = mongoose.model('User',{
   username : String,
   password : String,
   updated_at: { type: Date, default: Date.now },
-})
+});
 
 
 ///////////////////Auth
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
-app.get('/login', function (req, res)
-{
-    res.render('login.html');
-});
-app.post('/login',passport.authenticate('local', { successRedirect: '/',failureRedirect: '/login',failureFlash: true })
-);
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
+// var passport = require('passport'),LocalStrategy = require('passport-local').Strategy;
+//
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function(err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// ));
+// app.get('/login', function (req, res)
+// {
+//     res.render('login.html');
+// });
+// app.post('/login',passport.authenticate('local', { successRedirect: '/',failureRedirect: '/login',failureFlash: true })
+// );
+// app.get('/logout', function(req, res){
+//   req.logout();
+//   res.redirect('/');
+// });
 ///////////////
 
 //
